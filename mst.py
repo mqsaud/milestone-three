b09d8eb91305526a6eadbea359a7e960daa13ede
mqsaud <mqsaud@gmail.com>
2021-08-04 14:48:27 +0000

    Add logout function

--- mst.py
+++ mst.py
@@ -88,7 +88,19 @@ def profile(username):
     # grab the session user's name from database
     username = mongo.db.users.find_one(
               {"username": session["user"]})["username"]
-    return render_template("profile.html", username=username)
+
+    if session["user"]:
+        return render_template("profile.html", username=username)
+
+    return redirect(url_for("login"))
+
+
+@mst.route("/logout")
+def logout():
+    # remove user from session cookies
+    flash("You hav been logged out")
+    session.pop("user")
+    return redirect(url_for("login"))
 
 
 if __name__ == "__main__":
